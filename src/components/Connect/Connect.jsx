068815a65c9ac70756ff4dd158e790103b407f49
Imane-ux <iamin053@uottawa.ca>
2024-07-20 13:07:46 -0400

    Adding icons to assets folder and to connect section

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Connect.css'
 import { useTranslation } from 'react-i18next'
+import whatsappIcon from '../../assets/whatsApp-green.png' // Import your WhatsApp icon image
+import instagramIcon from '../../assets/instagram.png'
 
 const Connect = () => {
   const { t} = useTranslation();
@@ -10,10 +12,18 @@ const Connect = () => {
         <h2> {t("connectSection.subTitle")}</h2>
         <p>{t("connectSection.content1")}</p>
 
-            <a href="https://chat.whatsapp.com/D7EbDJjg4a02muPGDbak9p" style={{ textDecoration: 'underline', color: '#10453C' }}>WhatsApp Group Chat</a>
-            <br/>
-            <a href="https://www.instagram.com/j/AbYnSWEuxh7dxAIq/" style={{ textDecoration: 'underline', color: '#10453C' }}>Instagram Group Chat</a>
-
+        <div className="social-links">
+        <a href="https://chat.whatsapp.com/D7EbDJjg4a02muPGDbak9p" style={{ textDecoration: 'underline', color: '#10453C' }}>
+          <img src={whatsappIcon} alt="WhatsApp Icon" className="icon" />
+          WhatsApp Group Chat
+        </a>  
+        <br />
+        <a href="https://www.instagram.com/j/AbYnSWEuxh7dxAIq/" style={{ textDecoration: 'underline', color: '#10453C' }}>
+          <img src={instagramIcon} alt="Instagram Icon" className="icon" />
+          Instagram Group Chat
+        </a>
+      </div>
+      
         <br/>
         <br/>
         <p> {t("connectSection.content2")}</p>
